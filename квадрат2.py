f0a51d5d57f7adec61ac708e3cd8c8d6e148ce62
ppp999zzz <endzer9@gmail.com>
2024-08-08 17:51:01 +0700

    Add files via upload

--- квадрат2.py
+++ квадрат2.py
@@ -1,13 +1,24 @@
 import pygame
+import random
+
+class Rect:
+    def __init__(self,width,height):
+        self.x = random.randint(1,499)
+        self.y = 100
+        self.ex = random.randint(5,10)
+        self.width = width
+        self.height = height
 
 screen = pygame.display.set_mode((500, 500))
 x = 225
 y = 225
-z = 300
-v = 300
 dx = 1
+#random.randit(a, b) - рандомное число в диапазоне от а до b
+num_rects = random.randint(3, 5)
+#0 + 1 500 - ширина квадрата(а - 1)
+rects = [Rect(50,50) for i in range (num_rects)]
 while True:
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         pass
     keys = pygame.key.get_pressed()
@@ -19,18 +30,14 @@ while True:
         y -= 1
     if keys[pygame.K_s] and y <= 450:
         y += 1
-        
-        
-    screen.fill((0, 0, 0))
-    for event in pygame.event.get():
-        pass
-    if z >= 450 or z <= 0:
-        dx *= -1
-    pygame.draw.rect(screen, (225,0,0), (z, v, 50, 50))
-    z += dx
-
     
-    pygame.draw.rect(screen, (255, 255, 255), (x, y, 50, 50))
+    for i in range(len(rects)):
+        if rects[i].x >= 450 or rects[i].x <= 0:
+            rects[i].ex = -rects[i].ex
+        rects[i].x += rects[i].ex  
+        pygame.draw.rect(screen, (139,0,0), (rects[i].x, rects[i].y,
+                                             rects[i].width, rects[i].height))
+    pygame.draw.rect(screen, (255, 255, 255),(x,y, 50, 50))
     pygame.display.update()
-    pygame.time.delay(1)
+    pygame.time.delay(6)
     
